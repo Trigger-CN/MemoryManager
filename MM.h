1fa33ab34fcc85f6bced2f46ea922ecdd0aeb514
Trigger-CN <58842678+Trigger-CN@users.noreply.github.com>
2020-12-16 18:09:10 +0800

    Add files via upload

---
+++ MM.h
@@ -0,0 +1,23 @@
+#ifndef _MM_H
+#define _MM_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+
+void MM_MemPoolInit(void);
+uint16_t MM_GetTableIndex(void* Block);
+void* MM_Alloc(size_t s);
+uint8_t MM_Free(void* Block);
+uint8_t MM_Set(void *Block, int val, size_t s);
+int MM_Occupation(void);
+    
+#ifdef __cplusplus
+}// extern "C"
+#endif
+
+#endif
+
+
